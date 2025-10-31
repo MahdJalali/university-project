f35eeb4a01dbb7ea3bc2be7261d0d4aaf51bce54
MahdJalali <mjala0160@gmial.com>
2025-10-31 20:02:40 +0330

    new

---
+++ Calculate-Age.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+birth_year = int(input("Enter your birth year: "))
+
+
+current_year = datetime.now().year
+
+
+age_years = current_year - birth_year
+
+
+age_days = age_years * 365    
+age_hours = age_days * 24
+age_minutes = age_hours * 60
+age_seconds = age_minutes * 60
+
+
+print(f"\nYou are approximately {age_years} years old.")
+print(f"That’s about {age_hours:,} hours.")
+print(f"Or about {age_minutes:,} minutes.")
+print(f"And around {age_seconds:,} seconds.")
